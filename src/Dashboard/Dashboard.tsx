fcb86d2630804525f046b0e986272ef50b824d2c
Ante Milina <ante.milina@hotmail.com>
2023-02-04 12:10:39 +0100

    Refactor GET values

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,34 +1,55 @@
 import React from "react";
 import { useState } from "react";
-import Flightss from "../Data.tsx";
+import Flights from "../Data.tsx";
 import { Grid } from "@mui/material";
 import CustomButton from "../Fields/CustomButton.tsx";
+import NewFlightForm from "../Forms/NewFlightForm.tsx";
 
 const Dashboard = () => {
   const [createFlight, setCreateFlight] = useState(false);
 
-  const arrayValues = [];
+  interface IFlight {
+    key: string;
+    aircraftRegistration: string;
+    aircraftType: string;
+    dateArrival: string;
+    dateDeparture: string;
+    flightNumber: string;
+  }
 
-  Object.keys(Flightss).forEach((key) => {
-    arrayValues.push([key, Flightss[key]]);
+  const allFlights: IFlight = [];
+
+  Object.keys(Flights).forEach((key) => {
+    allFlights.push({
+      key: key,
+      aircraftRegistration: Flights[key].aircraftRegistration,
+      aircraftType: Flights[key].aircraftType,
+      dateArrival: Flights[key].dateArrival,
+      dateDeparture: Flights[key].dateDeparture,
+      flightNumber: Flights[key].flightNumber,
+    });
   });
 
   return (
     <React.Fragment>
       <Grid container spacing={5} padding={5}>
-        {arrayValues.map((flight) => (
-          <Grid key={flight[0]} item>
-            {"key: " + flight[0]}
+        {allFlights.map((flight) => (
+          <Grid key={flight.key} item>
+            {"key: " + flight.key}
             <br />
-            {"Registration: " + flight[1].aircraftRegistration} <br /> {" Type: " + flight[1].aircraftType}
-            <br /> {" Date Arrival: " + flight[1].dateArrival}
-            <br /> {" Date Departure: " + flight[1].dateDeparture}
-            <br /> {" Flight Number: " + flight[1].flightNumber}
+            {"Registration: " + flight.aircraftRegistration} <br /> {" Type: " + flight.aircraftType}
+            <br /> {" Date Arrival: " + flight.dateArrival}
+            <br /> {" Date Departure: " + flight.dateDeparture}
+            <br /> {" Flight Number: " + flight.flightNumber}
           </Grid>
         ))}
       </Grid>
       <CustomButton onClick={() => setCreateFlight((prevState) => !prevState)}>CREATE NEW FLIGHT</CustomButton>
-      {createFlight && <div>Form to be Added</div>}
+      {createFlight && (
+        <div>
+          <NewFlightForm></NewFlightForm>
+        </div>
+      )}
     </React.Fragment>
   );
 };
